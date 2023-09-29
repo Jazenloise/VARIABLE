b99d213d9640b2422a9504b543ad0a803fe038f8
Jazenloise <92614349+Jazenloise@users.noreply.github.com>
2023-09-29 17:46:27 +0800

    Add files via upload

---
+++ VARIABLE TYPES.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integers</title>
+</head>
+<body>
+    
+<?php
+
+$int_var1 = 1234;
+$another_int = -6789 + 1234;
+
+$sum = $int_var1 + $another_int;
+$difference = $int_var1 - $another_int;
+$product = $int_var1 * $another_int;
+$quotient = $int_var1 / $another_int;
+
+echo "Integer 1: $int_var1<br>";
+echo "Integer 2: $another_int<br>";
+echo "Sum: $sum<br>";
+echo "Difference: $difference<br>";
+echo "Product: $product<br>";
+echo "Quotient: $quotient<br>";
+
+?>
+</body>
+</html>
