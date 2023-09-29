21966e70bab3316c987606df92ad6525b12701ad
Jazenloise <92614349+Jazenloise@users.noreply.github.com>
2023-09-29 17:54:44 +0800

    .

---
+++ DOUBLE.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOUBLE</title>
+</head>
+<body>
+    <?php
+
+$many = 1.1234;
+$many_2 = 2.1234;
+
+$few = $many + $many_2;
+
+echo "$many + $many_2 = $few<br>";
+
+?>
+
+</body>
+</html>
