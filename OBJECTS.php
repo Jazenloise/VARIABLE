6957165fbb94abefaa6d51c64e287dd4f668ab6c
Jazenloise <92614349+Jazenloise@users.noreply.github.com>
2023-10-10 10:14:16 +0700

    first commit

---
+++ OBJECTS.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>objects</title>
+</head>
+<body>
+
+<?php
+class Human
+{
+    function do_Human()
+    {
+        echo "Mekusmekus mo na yan insan"; 
+    }
+}
+
+$bar = new Human;
+$bar->do_Human();
+?>
+
+</body>
+</html>
