36905089316b878b20e59ee2172009974abdb80c
Jazenloise <92614349+Jazenloise@users.noreply.github.com>
2023-10-10 10:07:49 +0700

    first commit

---
+++ ARRAY.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+
+<?php
+ $licudine = array("Sugar Mommy", "Money", "Food" , "Playing Games" ,);
+ echo "I like " . $licudine[0] . ", " . $licudine[1] . ", " . $licudine[2] . " and " . $licudine[3] . ".";
+?>
+
+</body>
+</html>
